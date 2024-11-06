d8c7a7b9336e1f57d8384fd8f7a053ffe7830937
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-11-05 21:45:10 -0300

    Add files via upload

---
+++ PROJETOS/PlayAgenda PostgreSQL/conf/evolutions/default/1.sql
@@ -0,0 +1,33 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table agenda (
+  nome                      varchar(255),
+  email                     varchar(255),
+  telefone                  varchar(255),
+  endereco                  varchar(255),
+  facebook                  varchar(255))
+;
+
+create table email (
+  sender                    varchar(255),
+  recipient                 varchar(255),
+  subject                   varchar(255),
+  message                   varchar(255))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table agenda;
+
+drop table email;
+
+SET FOREIGN_KEY_CHECKS=1;
+
