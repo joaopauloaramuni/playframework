5d13b09824117c5e9d6a764c31375f2b3451dd27
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-11-05 21:46:31 -0300

    Add files via upload

---
+++ PROJETOS/PlayAgenda MYSQL/app/models/Usuario.java
@@ -0,0 +1,29 @@
+package models;
+
+import play.db.ebean.*;
+import javax.persistence.*;
+
+@SuppressWarnings("serial")
+@Entity
+public class Usuario extends Model{
+	
+	public String nome;
+	public String email;
+	public String username;
+	public String password;
+	
+	public Usuario(String nome, String email, String username, String password)  {
+		this.nome = nome;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [nome=" + nome + ", email=" + email + ", username="
+				+ username + ", password=" + password + "]";
+	}
+
+}
+
